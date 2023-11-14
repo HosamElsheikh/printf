801dd0ac0e8ca3a358eba5a2f74a5dd2b7a41e7b
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2023-11-14 11:27:17 -0800

    fixing the damned betty

--- get_function.c
+++ get_function.c
@@ -11,27 +11,27 @@
 
 int get_function(char con_spec, va_list args)
 {
-        int i = 0;
-        int count_fun = 0;
+	int i = 0;
+	int count_fun = 0;
 
-        specifiers_t spec[] = {
-                {'c', handle_char},
-                {'s', handle_string},
-                {'d', handle_digit},
-                {'i', handle_digit},
-                {0, NULL}
-        };
+	specifiers_t spec[] = {
+		{'c', handle_char},
+		{'s', handle_string},
+		{'d', handle_digit},
+		{'i', handle_digit},
+		{0, NULL}
+	};
 
-        while (spec[i].specifiers)
-        {
-                if (con_spec == spec[i].specifiers)
-                        count_fun += spec[i].f(args);
-                i++;
-        }
+	while (spec[i].specifiers)
+	{
+		if (con_spec == spec[i].specifiers)
+			count_fun += spec[i].f(args);
+		i++;
+	}
 
-        if (count_fun == 0)
-        {
-                count_fun += _putchar('%');
+	if (count_fun == 0)
+	{
+		count_fun += _putchar('%');
 	}
 	return (count_fun);
 }
