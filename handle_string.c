e1d6ad938b75eaca28d18ec9a6874c8b0b4c1bd9
Mariam Mazen <42315208+Mariammazen147@users.noreply.github.com>
2023-11-14 20:31:24 +0200

    Update handle_string.c

--- handle_string.c
+++ handle_string.c
@@ -1,25 +1,29 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
- * handle_string - Handles the 's' conversion specifier for _printf
- * @args: The va_list containing the argument to be formatted
+ * handle_string - print string.
  *
- * Return: The number of characters printed
+ * @args: argument.
+ *
+ * Return: character count.
  */
+
 int handle_string(va_list args)
 {
-	char *str = va_arg(args, char *);
-	int count = 0;
+        int i;
+        int count_fun = 0;
+        char *str = va_arg(args, char *);
 
+        if (!str)
+                str = "(null)";
 
-	while (*str)
-	{
-		if (write(1, str, 1) == -1)
-		{
-			return -1;
-		}
-		str++;
-		count++;
-	}
-	return (count);
+        if (str[0] == '\0')
+                return (-1);
+
+        for (i = 0; str[i] != '\0'; i++)
+                count_fun += _putchar(str[i]);
+
+        return (count_fun);
 }
